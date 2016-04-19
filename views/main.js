ce597c61466c3bfe7e07a0bad8e606aa8a2d8ec2
Sihui Huang <sihuih@gmail.com>
2016-04-19 10:03:57 -0500

    add google any

--- views/main.js
+++ views/main.js
@@ -9,5 +9,12 @@ var name = data[0];
 var fb_id = data[1];
 //var comments = app.props.comments;
 //console.log("in main,js comments: " + comments);
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-76618743-1', 'auto');
+ga('send', 'pageview');
 
 ReactDOM.render(<Layout name={name} fb_id={fb_id}/>, app);
